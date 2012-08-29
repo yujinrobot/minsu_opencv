8f21c1929282912eb82499313b0c3a169fdb95cb
Kim Min Soo <ms.kim8717@gmail.com>
2012-08-29 20:59:37 +0900

    final update...

--- src/tracking_Redball_openCV.cpp
+++ src/tracking_Redball_openCV.cpp
@@ -56,7 +56,6 @@ public:
 
     image_pub_ = it_.advertise("out", 1);
     image_sub_ = it_.subscribe("camera/rgb/image_color", 1, &ImageConverter::imageCb, this);
-    //image_sub_ = it_.subscribe("in", 1, &ImageConverter::imageCb, this, image_transport::TransportHints("compressed"));
 
     pos_pub = nh_.advertise<geometry_msgs::Pose>("ball_info_pose",1);                                   // publish ball position
     depth_pos_sub = nh_.subscribe("depth_info_dist", 1, &ImageConverter::depthInfoDistCb, this);        // subscribe distance
@@ -82,77 +81,6 @@ public:
     cv::inRange(converted, cv::Scalar(0,160,180), cv::Scalar(2,255,255), result1);
     cv::inRange(converted, cv::Scalar(170,160,180),cv::Scalar(179,255,255),result2);
     cv::bitwise_or(result1, result2, result);
-
-    //cv::imshow("converted", converted);
-
-    /*
-    // for RGB color space
-    cv::Mat_<cv::Vec3b>::const_iterator it_rgb= frame.begin<cv::Vec3b>();
-
-    // for HSV color space
-    cv::Mat_<cv::Vec3b>::const_iterator it= converted.begin<cv::Vec3b>();
-    cv::Mat_<cv::Vec3b>::const_iterator itend= converted.end<cv::Vec3b>();
-    cv::Mat_<uchar>::iterator itout = result.begin<uchar>();
-
-    // RGB color space
-    int avg_red = 0;
-    int avg_green = 0;
-    int avg_blue = 0;
-
-
-    // HSV color space
-    int avg_hue = 0, oor = 0;
-    int avg_saturation = 0;
-    int avg_value = 0;
-
-    for(; it!=itend; ++it, ++itout) {
-
-      // RGB color space
-      avg_blue += (*it_rgb)[0];
-      avg_green += (*it_rgb)[1];
-      avg_red += (*it_rgb)[2];
-
-      // HSV color space
-      // range Hue              : 0 ~ 360
-      // range Saturation       : 0 ~ 100
-      // range Value(intensity) : 0 ~ 100
-
-      // 1 byte                 : 0 ~ 25
-      // real range Hue         : 0 ~ 180 (Hue/2)
-
-      avg_hue += (*it)[0];              // hue value
-      avg_saturation += (*it)[1];       // saturation value
-      avg_value += (*it)[2];            // value value
-
-      if ((*it)[0] > 180) oor++;
-
-    }
-
-    int avg_hue2 = 0;
-    int avg_saturation2 = 0;
-    int avg_value2 = 0;
-
-    cv::Rect roi = cv::Rect(320-40, 240-30, 80, 60);
-    cv::Mat cropped = converted(roi);
-    it= cropped.begin<cv::Vec3b>();
-    itend= cropped.end<cv::Vec3b>();
-
-    for(; it!=itend; ++it, ++itout) {
-      avg_hue2 += (*it)[0];              // hue value
-      avg_saturation2 += (*it)[1];       // saturation value
-      avg_value2 += (*it)[2];            // value value
-
-    }
-
-    std::cout << "R : " << avg_red/frame.size().area() << " " << "G : " << avg_green/frame.size().area() << " "
-              << "B : " << avg_blue/frame.size().area()<< std::endl;
-    //std::cout << avg_hue/converted.size().area() << "  " <<  oor << "\n";minDetect
-    std::cout << "H : " << avg_hue/converted.size().area() << " " << "S : " << avg_saturation/converted.size().area() << " "
-              << "V : " << avg_value/converted.size().area() << std::endl << std::endl;
-
-    std::cout << "H : " << avg_hue2/cropped.size().area() << " " << "S : " << avg_saturation2/cropped.size().area() << " "
-              << "V : " << avg_value2/cropped.size().area() << std::endl << std::endl;
-    */
 
     return result;
   }
@@ -235,27 +163,6 @@ public:
     // for saving the eroded frame
     clone_eroded = eroded.clone();
 
-
-    /*
-    // pre-smoothing improves Hough detector
-    cv::GaussianBlur(clone_eroded, clone_eroded, cv::Size(3,3), 1.5);
-
-    // hough circle algorithm
-    std::vector<cv::Vec3f> circles;
-    cv::HoughCircles(threshold_frame, circles, CV_HOUGH_GRADIENT, 2, 200, 200, 100, 0.01, 500);
-    std::vector<cv::Vec3f>::const_iterator itc = circles.begin();
-
-    while(itc!=circles.end()) {
-      std::cout << "hough algorithm" << std::endl;
-      cv::circle(cv_ptr->image, cv::Point( (*itc)[0], (*itc)[1]) , (*itc)[2], cv::Scalar(255,0,0), 20);
-      ++itc;
-    }
-    */
-
-    // for saving the result frame
-    //origin_result = result.clone();
-
-
     ros::Time contour_centroid_start = ros::Time::now();
     std::vector< std::vector<cv::Point> > contours;     // storage for the contours
     std::vector<cv::Vec4i> hierarchy;                   // hierachyminDetect
@@ -280,20 +187,6 @@ public:
         cv::RotatedRect ellipse_candidate = cv::fitEllipse( cv::Mat(contours[i]) );
         cv::ellipse( cv_ptr->image, ellipse_candidate, cv::Scalar(0,255,0), 2, 8 );
         no_ellipse ++;
-
-        // radius -> {(W+H)/2}/2
-        //double radius_i = (ellipse_candidate.size.height+ellipse_candidate.size.width)/4;
-
-        //std::cout << "height : " << std::setw(7) << ellipse_candidate.size.height
-        //    << " " << "width : " << std::setw(7) << ellipse_candidate.size.width
-        //    << " " << "radius : " << radius_i << std::endl;
-
-        //double f= 700;
-        //opencv_Distance = 3 * f / radius_i;
-        //std::cout << "kobuki find the ball | opencv_Distance : " << opencv_Distance << std::endl;
-        //printf("R | Z = %f | %f\n", radius_i, double(opencv_Distance) );
-
-
 
         // ball centroid
         cv::Moments mom = cv::moments(threshold_frame);
@@ -332,13 +225,6 @@ public:
       std::cout << "kobuki can't find the ball" << std::endl;
     }
 
-
-
-
-    //cmd_vel_command();
-
-    //cv::Rect rect(320-40, 240-30, 80, 60);
-    //cv::rectangle(cv_ptr->image, rect, cv::Scalar(0,0,255), 5);
     cv::imshow("origin", cv_ptr->image);
     //cv::imshow("threshold", threshold_frame);
     //cv::imshow("dilate", dilated);
@@ -353,83 +239,6 @@ public:
     std::cout << "threshold time                 : " << threshold_end - threshold_start << std::endl;
     std::cout << "find contour and centroid time : " << contour_centroid_end - contour_centroid_start << std::endl;
   }
-
-  /*
-  // based on depth information
-  void cmd_command()
-  {
-    geometry_msgs::Twist cmd;
-
-    if (depth_Distance < 2 && depth_Distance > minDetect) {
-      //cmd.linear.x = depth_Distance-0.5;
-      cmd.linear.x = 0.3;
-      if (robot_posX > width_center) {
-        //cmd.angular.z = -fabs(robot_posX - width_center)/100;
-        cmd.angular.z = -0.3;
-      } else if (robot_posX < width_center) {
-        //cmd.angular.z = fabs(robot_posX - width_center)/100;
-        cmd.angular.z = 0.3;
-      } else if (robot_posX == width_center) {
-        cmd.angular.z = 0.0;
-      }
-      cmd_vel_pub.publish(cmd);
-      std::cout << "depth_Distance : " << depth_Distance << " " << "detecting ball" << " " << "vel_x : " << cmd.linear.x << " " << "vel_z" << " " << cmd.angular.z << std::endl;
-    }
-
-    else if (depth_Distance < minDetect && depth_Distance > 0.65) {
-      std::cout << "depth_Distance : " << depth_Distance << " " << "stop command" << std::endl;
-      cmd_vel_pub.publish(geometry_msgs::Twist()); // zero msg
-      depth_Distance = 0;
-    }
-
-    else {
-      std::cout << "depth_Distance : " << depth_Distance << " " << "rotation" << std::endl;
-      cmd.angular.z = 0.5;
-      cmd_vel_pub.publish(cmd);
-    }
-  }
-  */
-
-  /*
-  // based on openCV
-  void cmd_command()
-  {
-    geometry_msgs::Twist cmd;
-
-    if (opencv_Distance < 200 && opencv_Distance > minDetect) {
-      cmd.linear.x = opencv_Distance/100 -0.7;
-      if (robot_posX > width_center) {
-        cmd.angular.z = -0.3;
-      } else if (robot_posX < width_center) {
-        cmd.angular.z = 0.3;
-      } else if (robot_posX == width_center) {
-        cmd.angular.z = 0.0;
-      }
-      cmd_vel_pub.publish(cmd);
-      std::cout << "opencv_Distance : " << opencv_Distance << " " << "detecting ball" << " " << "vel_x : " << cmd.linear.x << " " << "vel_z" << " " << cmd.angular.z << std::endl;
-    }
-
-    else if (opencv_Distance < minDetect && opencv_Distance > 70) {
-      std::cout << "opencv_Distance : " << opencv_Distance << " " << "stop command" << std::endl;
-      cmd_vel_pub.publish(geometry_msgs::Twist()); // zero msg
-      opencv_Distance = 0;
-    }
-
-    else {
-      std::cout << "opencv_Distance : " << opencv_Distance << " " << "rotation" << std::endl;
-      cmd.angular.z = 0.5;
-      cmd_vel_pub.publish(cmd);
-    }
-  }
-  */
-
-  // It is test function.
-  void test()
-  {
-    geometry_msgs::Twist cmd;
-    cmd.angular.z = 0.2;
-    cmd_vel_pub.publish(cmd);
-  }
 };
 
 int main(int argc, char** argv)
@@ -441,4 +250,3 @@ int main(int argc, char** argv)
   ros::spin();
   return 0;
 }
-
